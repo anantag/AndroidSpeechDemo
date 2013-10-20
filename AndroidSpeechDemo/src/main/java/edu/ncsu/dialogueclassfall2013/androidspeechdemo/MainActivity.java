f42a42a564bf9e38a3968f7c368eeb67b85860db
Chris Fox <chrisdavidfox@gmail.com>
2013-10-20 15:31:09 -0400

    Added speech recognition. Press "listen" and recognized speech will appear in the text box.

--- AndroidSpeechDemo/src/main/java/edu/ncsu/dialogueclassfall2013/androidspeechdemo/MainActivity.java
+++ AndroidSpeechDemo/src/main/java/edu/ncsu/dialogueclassfall2013/androidspeechdemo/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.ncsu.dialogueclassfall2013.androidspeechdemo;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -11,19 +14,65 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
     public final static String EXTRA_MESSAGE = "edu.ncsu.dialogueclassfall2013.androidspeechdemo.MESSAGE";
 
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1001;
+
+    public void checkVoiceRecognition() {
+        // Check if voice recognition is present
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
+                RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        if (activities.size() == 0) {
+            ((Button) findViewById(R.id.listen_button)).setEnabled(false);
+            Toast.makeText(this, "Voice recognizer not present",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
     /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    public void listen(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                ArrayList<String> textMatchList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if (!textMatchList.isEmpty()) {
+                    String firstMatch = textMatchList.get(0);
+                    EditText editText = (EditText) findViewById(R.id.edit_message);
+                    editText.setText(firstMatch);
+                }
+            }
+        } else if(resultCode == RecognizerIntent.RESULT_AUDIO_ERROR){
+            showToastMessage("Audio Error");
+        } else if(resultCode == RecognizerIntent.RESULT_CLIENT_ERROR){
+            showToastMessage("Client Error");
+        } else if(resultCode == RecognizerIntent.RESULT_NETWORK_ERROR){
+            showToastMessage("Network Error");
+        } else if(resultCode == RecognizerIntent.RESULT_NO_MATCH){
+            showToastMessage("No Match");
+        } else if(resultCode == RecognizerIntent.RESULT_SERVER_ERROR){
+            showToastMessage("Server Error");
+        }
+    }
+
+    /**
+     * Helper method to show the toast message
+     **/
+    void showToastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -36,6 +85,8 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        checkVoiceRecognition();
     }
 
 
