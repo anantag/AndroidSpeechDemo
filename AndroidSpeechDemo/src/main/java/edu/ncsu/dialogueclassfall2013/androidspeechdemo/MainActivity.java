d09c3856762ad159339998240abb0fae6dff78f1
Chris Fox <chrisdavidfox@gmail.com>
2013-10-20 00:37:49 -0400

    Finished part 1 of Android tutorial. Enter text, press Send, and see the text displayed in a second activity.

--- AndroidSpeechDemo/src/main/java/edu/ncsu/dialogueclassfall2013/androidspeechdemo/MainActivity.java
+++ AndroidSpeechDemo/src/main/java/edu/ncsu/dialogueclassfall2013/androidspeechdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.ncsu.dialogueclassfall2013.androidspeechdemo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,8 +11,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "edu.ncsu.dialogueclassfall2013.androidspeechdemo.MESSAGE";
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
